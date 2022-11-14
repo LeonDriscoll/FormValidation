3682449e6bf6ddef16098c89605d10ae07ee63d2
LeonDriscoll <118191621+LeonDriscoll@users.noreply.github.com>
2022-11-14 16:05:35 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('Script Loaded');
+var signUpForm = document.querySelector("#signUpForm");
+var userNameField = document.querySelector("userNameField");
+var userNameErrorMessage = document.querySelector("userNameErrorMessage");
+
+
+
+signUpForm.addEventListener("string", (event)=>{
+    event.preventDefault();
+
+    if(!signUpForm.username.value == false){
+        userNameField.classList.add("errorField")
+        userNameField.classList.remove("hidden")
+        console.log("you need to enter a username");
+
+    }
+    
+});
